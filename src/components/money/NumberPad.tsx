229c43b3c5ade43476cc1bf8605d20e925cb5e57
zheng <849859182@qq.com>
2020-08-31 21:54:36 +0800

    优化修改NumberPad组件的样式

--- src/components/money/NumberPad.tsx
+++ src/components/money/NumberPad.tsx
@@ -3,7 +3,13 @@ import React from "react";
 
 const NumberPadWrapper = styled.section`
   > .output {
-    height: 60px;
+    font-size: 36px;
+    line-height: 72px;
+    background: #fff;
+    text-align: right;
+    padding: 0 16px;
+    box-shadow: inset 0 -6px 5px -5px rgba(0, 0, 0, 0.25),
+      inset 0 6px 5px -5px rgba(0, 0, 0, 0.25);
   }
   > .buttons {
     &::after {
@@ -12,15 +18,51 @@ const NumberPadWrapper = styled.section`
       display: block;
     }
     > button {
+      font-size: 18px;
       float: left;
-      height: 60px;
+      height: 64px;
       width: 25%;
+      border: none;
       &.OK {
         float: right;
-        height: 120px;
+        height: 128px;
       }
       &.zero {
         width: 50%;
+      }
+      &:nth-child(1) {
+        background: #f2f2f2;
+      }
+
+      &:nth-child(2),
+      &:nth-child(5) {
+        background: #e0e0e0;
+      }
+
+      &:nth-child(3),
+      &:nth-child(6),
+      &:nth-child(9) {
+        background: #d3d3d3;
+      }
+
+      &:nth-child(4),
+      &:nth-child(7),
+      &:nth-child(10) {
+        background: #c1c1c1;
+      }
+
+      &:nth-child(8),
+      &:nth-child(11),
+      &:nth-child(13) {
+        background: #b8b8b8;
+      }
+
+      &:nth-child(14) {
+        background: #a9a9a9;
+      }
+
+      &:nth-child(12) {
+        background: #9a9a9a;
       }
     }
   }
@@ -28,7 +70,7 @@ const NumberPadWrapper = styled.section`
 function NumberPad() {
   return (
     <NumberPadWrapper>
-      <div className="output"></div>
+      <div className="output">100</div>
       <div className="buttons">
         <button>1</button>
         <button>2</button>
